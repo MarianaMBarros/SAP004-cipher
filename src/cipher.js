e9ee2668a61e01feba6bc62ce01c984137667844
Mariana Barros <mamirandabarros11@gmail.com>
2020-04-08 00:57:24 -0300

    ateraçao na formula da cifra e css melhorado

--- src/cipher.js
+++ src/cipher.js
@@ -1,72 +1,50 @@
-const alphabet = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
-];
-
 const cipher = {
-  encode: function(offset, message) {
+  encode: function (offset, message) {
     let newPhrase = "";
-    const phrase = message.toUpperCase();
-    const displace = offset;
-    const alphabetSize = 26;
-    for (let i = 0; i < phrase.length; i++) {
-      let letterAlphabet = phrase[i];
-      let position = alphabet.indexOf(letterAlphabet);
-      if (position == -1) {
-        newPhrase = `${newPhrase}${letterAlphabet}`;
-      } else {
-        let newPosition = (position + displace) % alphabetSize;
-        let newLetterAlphabet = alphabet[newPosition];
+
+    for (let i = 0; i < message.length; i++) {
+      let letterCode = message[i].charCodeAt();
+      // Maiuscula
+      if (letterCode >= 65 && letterCode <= 90) {
+        let newLetterCode = ((letterCode - 65 + offset) % 26) + 65;
+        let newLetterAlphabet = String.fromCharCode(newLetterCode);
         newPhrase = `${newPhrase}${newLetterAlphabet}`;
+        //Minuscula
+      } else if (letterCode >= 97 && letterCode <= 122) {
+        let newLetterCode = ((letterCode - 97 + offset) % 26) + 97;
+        let newLetterAlphabet = String.fromCharCode(newLetterCode);
+        newPhrase = `${newPhrase}${newLetterAlphabet}`;
+        //caracter especial
+      } else {
+        newPhrase = message;
+      }
+    }
+
+    return newPhrase;
+  },
+
+  decode: function (offset, message) {
+    let newPhrase = "";
+
+    for (let i = 0; i < message.length; i++) {
+      let letterCode = message[i].charCodeAt();
+      // Maiuscula
+      if (letterCode >= 65 && letterCode <= 90) {
+        let newLetterCode = ((letterCode - 90 - offset) % 26) + 90;
+        let newLetterAlphabet = String.fromCharCode(newLetterCode);
+        newPhrase = `${newPhrase}${newLetterAlphabet}`;
+        //Minuscula
+      } else if (letterCode >= 97 && letterCode <= 122) {
+        let newLetterCode = ((letterCode - 122 - offset) % 26) + 122;
+        let newLetterAlphabet = String.fromCharCode(newLetterCode);
+        newPhrase = `${newPhrase}${newLetterAlphabet}`;
+        //caracter especial
+      } else {
+        newPhrase = message;
       }
     }
     return newPhrase;
   },
-  decode: function(offset, message) {
-    let newPhrase = "";
-    const phrase = message.toUpperCase();
-    const displace = offset;
-    const alphabetSize = 26;
-    for (let i = 0; i < phrase.length; i++) {
-      let letterAlphabet = phrase[i];
-      let position = alphabet.indexOf(letterAlphabet);
-      if (position == -1) {
-        newPhrase = `${newPhrase}${letterAlphabet}`;
-      } else {
-        let newPosition =
-          (((position - displace + alphabetSize) % alphabetSize) +
-            alphabetSize) %
-          alphabetSize;
-        let newLetterAlphabet = alphabet[newPosition];
-        newPhrase = `${newPhrase}${newLetterAlphabet}`;
-      }
-    }
-    return newPhrase;
-  }
 };
 
 export default cipher;
